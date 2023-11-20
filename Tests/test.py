3eaaf00805be6048a3f47bfa0efd5675fe5f64a3
Andreas691667 <andreaskaag@hotmail.com>
2023-11-20 14:07:01 +0100

    formatted doc

--- Tests/test.py
+++ Tests/test.py
@@ -1,9 +1,10 @@
 import sys
+
 sys.path.insert(0, "./Vector_clocks")
 from VectorProcess import VectorProcess
 
 if __name__ == "__main__":
-    N = 2   # Number of processes
+    N = 2  # Number of processes
     # Create processes
     processes = [VectorProcess(i, N) for i in range(N)]
 
@@ -12,7 +13,6 @@ if __name__ == "__main__":
     events0 = [(5, "event1", 1), (10, "event2", 1)]
     events1 = [(6, "event3", 0), (11, "event4", 0)]
     E = [events0, events1]
-
 
     for i, e in enumerate(E):
         for ei in e:
--- Vector_clocks/VectorProcess.py
+++ Vector_clocks/VectorProcess.py
@@ -9,10 +9,10 @@ class VectorProcess:
         self.processes = []
         self.main_thread = Thread(target=self.main_loop, daemon=True)
         self.stop_worker = Event()
-        self.message_queue = Queue()    #queue of incoming message (payload, timestamp)
-        self.events_queue = Queue()     #queue of tuples: (payload, receiver_id, time)
+        self.message_queue = Queue()  # queue of incoming message (payload, timestamp)
+        self.events_queue = Queue()  # queue of tuples: (payload, receiver_id, time)
         self.start_time = 0
-        self.clock = [0] * n            #vector clock initialized to 0
+        self.clock = [0] * n  # vector clock initialized to 0
 
     def start_loop(self):
         """Start the process loop"""
@@ -23,7 +23,7 @@ class VectorProcess:
     def get_process(self, _id):
         """Get process object by id"""
         return self.processes[_id]
-    
+
     def enqueue_message(self, payload, timestamp):
         """Enqueue message to be processed by the state machine"""
         self.message_queue.put((payload, timestamp))
@@ -35,25 +35,24 @@ class VectorProcess:
             # Check event queue for events
             if not self.events_queue.empty():
                 event_time, event_payload, out_id = self.events_queue.queue[0]
-                if (time_delta >= event_time):
+                if time_delta >= event_time:
                     # Remove element
                     self.events_queue.get()
                     # Call send message with event and timestamp
                     self.send_message(event_payload, out_id)
-        
 
             # Check for incoming messages
             try:
                 payload, clock_in = self.message_queue.get(timeout=0.1)
-            
+
             # Empty
             except Empty:
                 pass
-            
+
             # Not empty
             else:
-                self.receive_message(payload, clock_in) 
-        
+                self.receive_message(payload, clock_in)
+
         print(f"Process {self._id} stopped \n")
 
     def receive_message(self, payload, timestamp):
@@ -63,8 +62,10 @@ class VectorProcess:
         for i, clock_old in enumerate(self.clock):
             self.clock[i] = max(clock_old, timestamp[i])
 
-        print(f"[{time.time()-self.start_time}] Process {self._id} received message {payload} and clock is now: {self.clock} \n")
-    
+        print(
+            f"[{time.time()-self.start_time}] Process {self._id} received message {payload} and clock is now: {self.clock} \n"
+        )
+
     def send_message(self, payload, out_id):
         """Send message to another process"""
         if payload == "STOP":
@@ -73,8 +74,8 @@ class VectorProcess:
 
         self.clock[self._id] += 1
         # send message to random process
-        process : VectorProcess = self.get_process(out_id)
+        process: VectorProcess = self.get_process(out_id)
         process.enqueue_message(payload, self.clock)
-        print(f"[{time.time()-self.start_time}] Process {self._id} sent message {payload} with clock: {self.clock} to process {out_id} \n")
-
-    
+        print(
+            f"[{time.time()-self.start_time}] Process {self._id} sent message {payload} with clock: {self.clock} to process {out_id} \n"
+        )
